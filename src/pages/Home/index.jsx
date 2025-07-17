436e81e35c501a48832769b15982b57343fa4c54
hiro <hiroto1590@gmail.com>
2025-07-18 03:33:02 +0900

    feat: 홈화면

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,13 @@
+import MainSection from '@/components/MainSection';
+import EmojiSection from '@/components/EmojiSection';
+import BottomCTA from '@/components/BottomCTA';
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <main>
+      <MainSection />
+      <EmojiSection />
+      <BottomCTA />
+    </main>
+  );
 }
