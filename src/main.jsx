28b624f1c0fd6a4796420564c2aaf36fb5c9f47c
josubeen <whtnqls987@gmail.com>
2025-07-17 19:09:52 +0900

    chore: main.jsx 수정 내용 롤백

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,32 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import MessagePage from '@/pages/Message/index';
+import MainLayout from '@/layouts/MainLayout';
+import PostPageLayout from '@/layouts/PostPageLayout';
+import Home from '@/pages/Home';
+import ListPage from '@/pages/list/ListPage';
+import NotFound from '@/pages/NotFound';
+import PostDetail from '@/pages/PostDetail';
+
+import '@/styles/reset.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <MessagePage />
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/list' element={<ListPage />} />
+          <Route path='/post' element={<NotFound />} />
+          <Route path='/post/:id/message' element={<NotFound />} />
+        </Route>
+        <Route element={<PostPageLayout />}>
+          <Route path='/post/:id' element={<PostDetail />} />
+          <Route path='/post/:id/edit' element={<NotFound />} />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
